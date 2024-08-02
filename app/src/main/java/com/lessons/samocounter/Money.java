cefac7f8ad985a59f3993e035e8264fc9400ecfb
Jimmy2kurtki <v4vkurse@gmail.com>
2024-08-02 21:16:06 +0800

    сеатель бабок и сим по датам

--- app/src/main/java/com/lessons/samocounter/Money.java
+++ app/src/main/java/com/lessons/samocounter/Money.java
@@ -1,5 +1,6 @@
 package com.lessons.samocounter;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,13 +9,23 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
+import java.util.Locale;
 import java.util.Set;
 
 public class Money extends AppCompatActivity {
+
+    Date currentDate = new Date();
+    DateFormat dateFormat = new SimpleDateFormat("dd.MM", Locale.getDefault());
+    String dateText = dateFormat.format(currentDate);
 
     DBHelper dbHelper; //БДшка
 
@@ -28,17 +39,30 @@ public class Money extends AppCompatActivity {
 
     ArrayAdapter<String> adapterSpinnerSimFinish;//адаптер для спиннера правого
 
-    TextView textViewMoneyAtDates, textViewMoneyPerDay;
+    TextView textViewMoneyAtDates, textViewMoneyPerDay, textViewSimAtDates, textViewSimPerDay;
 
     Spinner spinnerStartDate, spinnerFinishDate;
 
-
-
     String startDate;
 
+    String finishDate = dateText;
 
-    String finishDate;
+    int indexDataStart;
 
+    int indexDataFinish;
+
+    int totalMoney;
+
+    int moneyPerDay;
+
+    int totalSim;
+
+    int simPerDay;
+
+    int indexStart = 0;
+    int indexFinish = 0;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +70,10 @@ public class Money extends AppCompatActivity {
 
         dbHelper = new DBHelper(this);
 
+        textViewMoneyAtDates = findViewById(R.id.textView_money_at_dates);
+        textViewMoneyPerDay = findViewById(R.id.textView_money_per_day);
+        textViewSimAtDates = findViewById(R.id.textView_sim_at_date);
+        textViewSimPerDay = findViewById(R.id.textView_sim_per_day);
 
         getData();
 
@@ -64,9 +92,14 @@ public class Money extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
+                totalMoney = 0;
+                moneyPerDay = 0;
                 //задаем в строку startDate выбор
                 setStartDate(i);
-                pickDates(getStartDate(),getFinishDate());
+
+                pickSimForDates(getStartDate(),getFinishDate(),getIndexDataStart(),getIndexDataFinish());
+
+                setTextViewMoney();
 
             }
 
@@ -81,9 +114,15 @@ public class Money extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
+                totalMoney = 0;
+                moneyPerDay = 0;
+
                 //задаем в строку finishDate выбор
                 setFinishDate(i);
-                pickDates(getStartDate(),getFinishDate());
+
+                pickSimForDates(getStartDate(),getFinishDate(),getIndexDataStart(),getIndexDataFinish());
+
+                setTextViewMoney();
 
             }
 
@@ -93,37 +132,133 @@ public class Money extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void setTextViewMoney(){
+        textViewMoneyAtDates.setText(String.valueOf(totalMoney));
+        textViewMoneyPerDay.setText(String.valueOf(moneyPerDay));
+        textViewSimAtDates.setText(String.valueOf(totalSim));
+        textViewSimPerDay.setText(String.valueOf(simPerDay));
+    }
+
+    public void pickSimForDates(String start,String finish,int indexDataStart, int indexDataFinish){
+
+        ArrayList<String> al = new ArrayList<>(arrayListSim);
+
+        Collections.reverse(al);
+
+        String[] arr = new String[al.size()];
+
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = al.get(i);
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            String s = arr[i];
+            boolean contains = s.contains(start);
+            if(contains){
+                indexStart = i;
+                break;
+            }
+        }
+
+        for (int i = arr.length-1; i > 0; i--){
+            String s = arr[i];
+            boolean contains = s.contains(finish);
+            if(contains){
+                indexFinish = i+1;
+                break;
+            }
+        }
+
+        if (indexStart <= indexFinish) {
+            String[] cutListSim = Arrays.copyOfRange(arr, indexStart, indexFinish);
+
+            if (indexDataStart <= indexDataFinish) {
+                String[] arr1 = new String[arrayListForSpinnerStart.size()];
+
+                for (int i = 0; i < arr1.length; i++) {
+                    arr1[i] = arrayListForSpinnerStart.get(i);
+                }
+
+
+                String[] cutListSpinner = Arrays.copyOfRange(arr1, indexDataStart, indexDataFinish + 1);
+
+                circleForCutListSim(cutListSim, cutListSpinner);
+            }
+
+        }
+    }
+
+    public void circleForCutListSim(String[] cutListSim, String[] cutListSpinner) {
+        int countSimInTotalSim = 0;
+        ArrayList<Integer> countSimAtDates = new ArrayList<>();
+
+        for (int i = 0; i < cutListSpinner.length; i++) {
+
+            int countSim = 0;
+
+            for (int x = 0; x < cutListSim.length; x++){
+
+                boolean contains = cutListSim[x].contains(cutListSpinner[i]);
+                if (contains) {
+                    countSim++;
+                    countSimInTotalSim++;
+                }
+            }
+
+            if(countSim != 0) {
+                countSimAtDates.add(countSim);
+            }
+        }
+        sumMoney(countSimAtDates);
+        totalSim = countSimInTotalSim;
+
+        if (countSimAtDates.size() != 0) {
+            simPerDay = totalSim / countSimAtDates.size();
+        }
+    }
+
+    public void sumMoney(ArrayList<Integer> countSimAtDates) {
+
+        for (int i = 0; i < countSimAtDates.size(); i++) {
+
+            countSimAtDates.set(i, moneyCount(countSimAtDates.get(i)));
+
+        }
+
+        for (int i = 0; i < countSimAtDates.size(); i++) {
+
+            totalMoney = totalMoney + countSimAtDates.get(i);
+
+        }
+
+        if (countSimAtDates.size() != 0) {
+            moneyPerDay = totalMoney / countSimAtDates.size();
+        }
 
     }
 
-    void setStartDate(int i){
-        startDate = arrayListForSpinnerStart.get(i);
+    public int moneyCount(int count) {
+        int intMoney;
+        if (count <= 23) {
+            intMoney = count * 146;
+        } else if (count <= 30) {
+            intMoney = ((count - 23) * 190) + (23 * 146);
+        } else if (count <= 33) {
+            intMoney = (7 * 190) + (23 * 146) + ((count - 23 - 7) * 210);
+        } else {
+            intMoney = (3 * 210) + (7 * 190) + (23 * 146) + ((count - 23 - 7 - 3) * 230);
+        }
+        return intMoney;
     }
-
-    public String getStartDate() {
-        return startDate;
-    }
-
-    public void setFinishDate(int i) {
-        finishDate = arrayListForSpinnerFinish.get(i);
-    }
-
-    public String getFinishDate() {
-        return finishDate;
-    }
-
-    public void pickDates(String start,String finish){
-
-
-    }
-
 
     void getData() {
         LinkedList<Data> list = dbHelper.GetAll();
         String text = "";
         String dateText = "";
-        for(Data d:list) text = text + d.nameSim + " " + d.emh + " " + d.date + "\n";
-        for(Data d:list) dateText = dateText + d.date + "\n";
+        for (Data d : list) text = text + d.nameSim + " " + d.emh + " " + d.date + "\n";
+        for (Data d : list) dateText = dateText + d.date + "\n";
 
         String[] arr = text.split("\n");
         Collections.addAll(arrayListSim, arr);
@@ -138,7 +273,6 @@ public class Money extends AppCompatActivity {
 
         arrayListForSpinnerFinish.clear();
         arrayListForSpinnerFinish.addAll(arrayListForSpinnerStart);
-        Collections.reverse(arrayListForSpinnerFinish);
     }
 
     public void onBackPressed() {
@@ -151,4 +285,33 @@ public class Money extends AppCompatActivity {
 
         }
     }
+
+    void setStartDate(int i) {
+        startDate = arrayListForSpinnerStart.get(i);
+        indexDataStart = i;
+    }
+
+    public String getStartDate() {
+
+        return startDate;
+    }
+
+    public int getIndexDataStart() {
+        return indexDataStart;
+    }
+
+    public void setFinishDate(int i) {
+        finishDate = arrayListForSpinnerFinish.get(i);
+        indexDataFinish = i;
+    }
+
+    public String getFinishDate() {
+
+        return finishDate;
+    }
+
+    public int getIndexDataFinish() {
+        return indexDataFinish;
+    }
+
 }
